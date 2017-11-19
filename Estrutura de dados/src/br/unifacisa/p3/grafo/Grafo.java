f19af17c76a40b9bb008b27444d0fd710ad0f617
Claudiano <claudianosampaio@gmail.com>
2017-11-18 23:06:15 -0300

    adicionando grafos

--- Estrutura de dados/src/br/unifacisa/p3/grafo/Grafo.java
+++ Estrutura de dados/src/br/unifacisa/p3/grafo/Grafo.java
@@ -1,10 +1,7 @@
 package br.unifacisa.p3.grafo;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
-import java.util.Vector;
 
 public class Grafo {
 
@@ -84,39 +81,7 @@ public class Grafo {
 		return false;
 		
 	}
-
-	/*
-	 * private int numeroDeVertices; private List<Integer>[] adjacentes;
-	 * 
-	 * public Grafo(int numeroDeVertices){ this.numeroDeVertices =
-	 * numeroDeVertices + 1; adjacentes = new List [this.numeroDeVertices]; }
-	 * 
-	 * public void adicionaAresta(int v1, int v2){ adjacentes[v1].add(v2); }
-	 * 
-	 * public void dfs(int verticeAlvo){ Stack<Integer> pilha = null; boolean
-	 * visitados[] = new boolean[numeroDeVertices];
-	 * 
-	 * //inicia array for (int i = 0; i < visitados.length; i++) { visitados[i]
-	 * = false; }
-	 * 
-	 * while(true){
-	 * 
-	 * if(!visitados[verticeAlvo]){ System.out.println("visitando: " +
-	 * verticeAlvo); visitados[verticeAlvo] = true; pilha.push(verticeAlvo); }
-	 * 
-	 * boolean achou = false; for (int i = 0; i <
-	 * adjacentes[verticeAlvo].size(); i++) { //atualiza o vertice alvo
-	 * if(!visitados[i]){ achou = true; verticeAlvo = i; break; } }
-	 * 
-	 * if(!achou){ pilha.pop(); //parar o while if(pilha.isEmpty()){ break; }
-	 * 
-	 * verticeAlvo = pilha.lastElement(); }
-	 * 
-	 * 
-	 * }
-	 * 
-	 * }
-	 * 
-	 */
+	
+	
 
 }
